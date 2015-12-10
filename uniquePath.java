6577ef4ab227b9a32f65882e3d42ff15ddfd9fe8
Yunyun Chen <cyy427@users.noreply.github.com>
2015-12-10 12:43:35 -0500

    Create uniquePath.java

---
+++ uniquePath.java
@@ -0,0 +1,10 @@
+    public int uniquePaths(int m, int n) {
+        if(m>n) return uniquePaths(n,m);
+        int[] cur = new int[m];
+        for(int i = 0; i < m; i++) 
+            cur[i] = 1;
+        for(int j = 1; j < n; j++) 
+            for(int i = 1; i < m; i++)
+                cur[i] += cur[i-1];
+        return cur[m-1];
+    }
