70a305bf895e7d0ce0f656c51f60adba86aa33af
cyy427 <yunyun.chen1992@gmail.com>
2016-07-24 20:09:20 -0700

    Jul 24

---
+++ 278_First_Bad_Version.py
@@ -0,0 +1,20 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+def isBadVersion(version):
+    pass
+
+class Solution(object):
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        l, r = 0, n
+        while l < r:
+            mid = l + (r-l)/2
+            if isBadVersion(mid):
+                r = mid
+            else:
+                l = mid + 1
+        return r
--- 374_Guess_Number_Higher_or_Lower.py
+++ 374_Guess_Number_Higher_or_Lower.py
@@ -1,7 +1,11 @@
 # The guess API is already defined for you.
 # @param num, your guess
 # @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
-# def guess(num):
+
+
+def guess(num):
+    pass
+
 
 class Solution(object):
     def guessNumber(self, n):
@@ -10,11 +14,12 @@ class Solution(object):
         :rtype: int
         """
         l, r = 0, n
-        mid = l + (r - l)/2
-        while guess(mid) != 0:
+        while l <= r:
+            mid = l + (r - l)/2
             if guess(mid) == 1:
                 l = mid + 1
-            else:
+            elif guess(mid) == -1:
                 r = mid - 1
-            mid = l + (r - l)/2
+            else:
+                return mid
         return mid
